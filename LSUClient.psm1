75d770f50b21a818b751c99c8ea33ab2a9c74f7c
jantari <jantari@github.com>
2020-11-12 15:22:04 +0100

    fix typo, stop screaming and bump mentioned powershell version to 7.1 as it works there too

--- LSUClient.psm1
+++ LSUClient.psm1
@@ -13,9 +13,9 @@ enum DependencyParserState {
     DO_NOT_HAVE = 1
 }
 
-# Check for old Windows versions in a manner that is compatible with PowerShell 2.0 all the way to to 7.0
-$WINDOWSVERSION = (New-Object -TypeName 'System.Management.ManagementObjectSearcher' -ArgumentList "SELECT Version FROM Win32_OperatingSystem").Get() | Select-Object -ExpandProperty Version
-if ($WINDOWSVERSION -notmatch "^10\.") {
+# Check for old Windows versions in a manner that is compatible with PowerShell 2.0 all the way up to 7.1
+$WindowsVersion = (New-Object -TypeName 'System.Management.ManagementObjectSearcher' -ArgumentList "SELECT Version FROM Win32_OperatingSystem").Get() | Select-Object -ExpandProperty Version
+if ($WindowsVersion -notlike "10.*") {
     throw "This module requires Windows 10."
 }
 
