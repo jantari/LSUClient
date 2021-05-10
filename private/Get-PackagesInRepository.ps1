a66b3e7f76955170d74495a86a4d137d70fa857b
jantari <jantari@github.com>
2021-05-10 22:13:45 +0200

    consolidate a chunk of duplicate code

--- private/Get-PackagesInRepository.ps1
+++ private/Get-PackagesInRepository.ps1
@@ -44,53 +44,37 @@ function Get-PackagesInRepository {
 
             # Downloading with Net.WebClient seems to remove the BOM automatically, this only seems to be neccessary when downloading with IWR. Still I'm leaving it in to be safe
             [xml]$PARSEDXML = $COMPUTERXML -replace "^$UTF8ByteOrderMark"
-
-            foreach ($Package in $PARSEDXML.packages.package) {
-                $PathInfo = Get-PackagePathInfo -Path $Package.location -BasePath $Repository
-                if ($PathInfo.Reachable) {
-                    Write-Debug "Found package: $($PathInfo.AbsoluteLocation)"
-                    [PSCustomObject]@{
-                        XMLFullPath  = $PathInfo.AbsoluteLocation
-                        XMLFile      = $Package.location -replace '^.*[\\/]'
-                        Directory    = $PathInfo.AbsoluteLocation -replace '[^\\/]*$'
-                        Category     = $Package.category
-                        LocationType = $PathInfo.Type
-                    }
-                } else {
-                    Write-Error "The package definition at $($Package.location) could not be found or accessed"
-                }
-            }
         } elseif ($RepositoryType -eq 'FILE') {
             # Model XML method for file based repositories
-            if (Test-Path -Path (Join-Path -Path $Repository -ChildPath "${Model}_Win10.xml") -PathType Leaf) {
-                $COMPUTERXML = Get-Content (Join-Path -Path $Repository -ChildPath "${Model}_Win10.xml") -Raw
+            $COMPUTERXML = Get-Content (Join-Path -Path $Repository -ChildPath "${Model}_Win10.xml") -Raw
 
-                # Strings with a BOM cannot be cast to an XmlElement, so we make sure to remove it if present
-                [xml]$PARSEDXML = $COMPUTERXML -replace "^$UTF8ByteOrderMark"
+            # Strings with a BOM cannot be cast to an XmlElement, so we make sure to remove it if present
+            [xml]$PARSEDXML = $COMPUTERXML -replace "^$UTF8ByteOrderMark"
+        }
 
-                foreach ($Package in $PARSEDXML.packages.package) {
-                    $PathInfo = Get-PackagePathInfo -Path $Package.location -BasePath $Repository
-                    if ($PathInfo.Reachable) {
-                        Write-Debug "Found packag: $($PathInfo.AbsoluteLocation)"
-                        [PSCustomObject]@{
-                            XMLFullPath  = $PathInfo.AbsoluteLocation
-                            XMLFile      = $Package.location -replace '^.*[\\/]'
-                            Directory    = $PathInfo.AbsoluteLocation -replace '[^\\/]*$'
-                            Category     = $Package.category
-                            LocationType = $PathInfo.Type
-                        }
-                    } else {
-                        Write-Error "The package definition at $($Package.location) could not be found or accessed"
-                    }
+        foreach ($Package in $PARSEDXML.packages.package) {
+            $PathInfo = Get-PackagePathInfo -Path $Package.location -BasePath $Repository
+            Write-Host "Repo: $Repository,PkgLocation: $($Package.location), PkgInfo: $PathInfo"
+            if ($PathInfo.Reachable) {
+                Write-Debug "Found package: $($PathInfo.AbsoluteLocation)"
+                [PSCustomObject]@{
+                    XMLFullPath  = $PathInfo.AbsoluteLocation
+                    XMLFile      = $Package.location -replace '^.*[\\/]'
+                    Directory    = $PathInfo.AbsoluteLocation -replace '[^\\/]*$'
+                    Category     = $Package.category
+                    LocationType = $PathInfo.Type
                 }
+            } else {
+                Write-Error "The package definition at $($Package.location) could not be found or accessed"
             }
         }
     } elseif ((Get-PackagePathInfo -Path $DatabaseXmlPath).Reachable) {
         # database.xml method
-        # NOT IMPLEMENTED
+        # NOT IMPLEMENTED YET
+        throw [System.NotImplementedException]::new()
     } else {
-        # "Simply searching for subfolders with XMLs inside them"-method
-        # This should be a fallback method only - it only works for filesystem
+        # "Simply searching for subfolders with XMLs inside them" fallback method.
+        # This should be a last ditch effort method only - it only works for filesystem
         # repositories and it cannot recover the categories of the packages.
         if ($RepositoryType -eq 'FILE') {
             Get-ChildItem -LiteralPath $Repository -Directory |
