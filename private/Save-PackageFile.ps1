4680bfced8df83e3e1867b9ed7663ffee836e773
jantari <jantari@github.com>
2021-07-30 18:33:04 +0200

    fix Install-LSUpdate redownloading installers from http repositories when they were already previously downloaded
    This new regression was particularly problematic for environments that
    require proxy settings for LSUClient to access the HTTP repository. In
    previous versions of LSUClient, Install-LSUpdate did not have any proxy
    parameters and existing scripts will not expect it to try to connect to
    the internet when all needed filess were previously downloaded with e.g.
    Save-LSUpdate (which did take proxy parameters)

--- private/Save-PackageFile.ps1
+++ private/Save-PackageFile.ps1
@@ -20,8 +20,18 @@
     )
 
     if ($SourceFile.Container -eq $Directory) {
-        # File is already in the destination location
+        # SourceFile is already in the destination
         return $SourceFile.AbsoluteLocation
+    }
+
+    # Test whether the file was previously downloaded to the destination (e.g. with Save-LSUpdate)
+    # This check is important for backwards compatibility with scripts written for LSUClient 1.2.5
+    # and earlier where Install-LSUpdate did not have proxy parameters so downloading first with
+    # Save-LSUpdate was recommended - Install-LSUpdate then has to work completely "offline" and use those
+    # files, or it would throw a connection error in environments where a proxy is required to download.
+    [string]$DownloadDest = Join-Path -Path $Directory -ChildPath $SourceFile.Name
+    if (Test-Path -LiteralPath $DownloadDest) {
+        return $DownloadDest
     }
 
     if (-not (Test-Path -Path $Directory)) {
@@ -32,7 +42,6 @@
         # Valid URL - Downloading file via HTTP
         $webClient = New-WebClient -Proxy $Proxy -ProxyCredential $ProxyCredential -ProxyUseDefaultCredentials $ProxyUseDefaultCredentials
 
-        [string]$DownloadDest = Join-Path -Path $Directory -ChildPath $SourceFile.Name
         Write-Verbose "Downloading '$($SourceFile.AbsoluteLocation)' to '${DownloadDest}'"
         $webClient.DownloadFile($SourceFile.AbsoluteLocation, $DownloadDest)
 
