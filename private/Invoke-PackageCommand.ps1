7ffb71188b47ba8b7f1013f21e1eeaaacf5c44f0
jantari <jantari@github.com>
2021-08-23 22:36:13 +0200

    get rid of Start-ProcessInRunspace function and move everything back into Invoke-PackageCommand
    This keeps the related code together (Start-ProcessInRunspace was only
    used by Invoke-PackageCommand) and minimizes the chance of a cmdlet or
    alias name conflict since Start-ProcessInRunspace is a fairly generic
    function name that could also be used by another module.

--- private/Invoke-PackageCommand.ps1
+++ private/Invoke-PackageCommand.ps1
@@ -31,30 +31,133 @@
     }
 
     $ExeAndArgs.Arguments = Remove-CmdEscapeCharacter -String $ExeAndArgs.Arguments
-
     Write-Debug "Starting external process:`r`n  File: $($ExeAndArgs.Executable)`r`n  Arguments: $($ExeAndArgs.Arguments)`r`n  WorkingDirectory: $Path"
-    $RunspaceOutput = Start-ProcessInRunspace -Executable $ExeAndArgs.Executable -Arguments $ExeAndArgs.Arguments -WorkingDirectory $Path -FallbackToShellExecute:$FallbackToShellExecute
-    $RunspaceOutput | Format-List | Out-Host
 
-    if ($RunspaceOutput.StdOutStream.Count -ne 1) {
-        Write-Warning "Unexpected results: None or more than 1 object returned: $($RunspaceOutput.StdOutStream)"
+    $Runspace = [System.Management.Automation.Runspaces.RunspaceFactory]::CreateOutOfProcessRunspace($null)
+    $Runspace.Open()
+
+    $Powershell = [PowerShell]::Create().AddScript{
+        Param (
+            [ValidateNotNullOrEmpty()]
+            [string]$WorkingDirectory,
+            [ValidateNotNullOrEmpty()]
+            [Parameter( Mandatory = $true )]
+            [string]$Executable,
+            [string]$Arguments,
+            [switch]$FallbackToShellExecute
+        )
+
+        Set-StrictMode -Version 3.0
+
+        # This value is used to communicate problems and errors that can be handled and or remedied/retried
+        # internally to the calling function. It stays 0 when no known errors occurred.
+        $HandledError = 0
+        $ProcessStarted = $false
+        [string[]]$StdOutLines = @()
+        [string[]]$StdErrLines = @()
+
+        $process                                  = [System.Diagnostics.Process]::new()
+        $process.StartInfo.WindowStyle            = [System.Diagnostics.ProcessWindowStyle]::Hidden
+        $process.StartInfo.UseShellExecute        = $false
+        $process.StartInfo.WorkingDirectory       = $WorkingDirectory
+        $process.StartInfo.FileName               = $Executable
+        $process.StartInfo.Arguments              = $Arguments
+        $process.StartInfo.RedirectStandardOutput = $true
+        $process.StartInfo.RedirectStandardError  = $true
+
+        if ($FallbackToShellExecute) {
+            $process.StartInfo.UseShellExecute        = $true
+            $process.StartInfo.RedirectStandardOutput = $false
+            $process.StartInfo.RedirectStandardError  = $false
+        }
+
+        try {
+            if (-not $process.Start()) {
+                $HandledError = 1
+            } else {
+                $ProcessStarted = $true
+            }
+        }
+        catch {
+            # In case we get ERROR_ELEVATION_REQUIRED (740) retry with ShellExecute to elevate with UAC
+            if ($null -ne $_.Exception.InnerException -and $_.Exception.InnerException.NativeErrorCode -eq 740) {
+                $HandledError = 740
+            # In case we get ERROR_BAD_EXE_FORMAT (193) retry with ShellExecute to open files like MSI
+            } elseif ($null -ne $_.Exception.InnerException -and $_.Exception.InnerException.NativeErrorCode -eq 193) {
+                $HandledError = 193
+            } else {
+                Write-Error $_
+            }
+        }
+
+        if ($ProcessStarted) {
+            if (-not $FallbackToShellExecute) {
+                # When redirecting StandardOutput or StandardError you have to start reading the streams asynchronously, or else it can cause
+                # programs that output a lot (like package u3aud03w_w10 - Conexant USB Audio) to fill a stream and deadlock/hang indefinitely.
+                # See issue #25 and https://stackoverflow.com/questions/11531068/powershell-capturing-standard-out-and-error-with-process-object
+                $StdOutAsync = $process.StandardOutput.ReadToEndAsync()
+                $StdErrAsync = $process.StandardError.ReadToEndAsync()
+            }
+
+            $process.WaitForExit()
+
+            if (-not $FallbackToShellExecute) {
+                $StdOutInOneString = $StdOutAsync.GetAwaiter().GetResult()
+                $StdErrInOneString = $StdErrAsync.GetAwaiter().GetResult()
+
+                [string[]]$StdOutLines = $StdOutInOneString.Split(
+                    [string[]]("`r`n", "`r", "`n"),
+                    [StringSplitOptions]::None
+                )
+
+                [string[]]$StdErrLines = $StdErrInOneString.Split(
+                    [string[]]("`r`n", "`r", "`n"),
+                    [StringSplitOptions]::None
+                )
+            }
+        }
+
+        return [PSCustomObject]@{
+            'StandardOutput' = $StdOutLines
+            'StandardError'  = $StdErrLines
+            'ExitCode'       = $process.ExitCode
+            'Runtime'        = $process.ExitTime - $process.StartTime
+            'HandledError'   = $HandledError
+        }
+    }
+
+    [void]$Powershell.AddParameters(@{
+        'WorkingDirectory'       = $Path
+        'Executable'             = $ExeAndArgs.Executable
+        'Arguments'              = $ExeAndArgs.Arguments
+        'FallbackToShellExecute' = $FallbackToShellExecute
+    })
+
+    $Powershell.Runspace = $Runspace
+    $RunspaceStandardOut = $Powershell.Invoke()
+
+    if ($RunspaceStandardOut.Count -ne 1) {
+        Write-Warning "Unexpected results: None or more than 1 object returned: $($RunspaceStandardOut)"
         return $null
     }
 
     # Print any unhandled / unexpected errors as warnings
-    if ($RunspaceOutput.StdErrStream.Count -gt 0) {
-        foreach ($ErrorRecord in $RunspaceOutput.StdErrStream) {
+    if ($PowerShell.Streams.Error.Count -gt 0) {
+        foreach ($ErrorRecord in $PowerShell.Streams.Error.ReadAll()) {
             Write-Warning $ErrorRecord
         }
     }
 
-    switch ($RunspaceOutput.StdOutStream[0].HandledError) {
+    $PowerShell.Runspace.Dispose()
+    $PowerShell.Dispose()
+
+    switch ($RunspaceStandardOut[0].HandledError) {
         # Success case
         0 {
             $RunspaceOutput.StdOutStream[0] | Format-List | Out-Host
             return $RunspaceOutput.StdOutStream[0]
         }
-        # Error cases that are handled explicitly
+        # Error cases that are handled explicitly inside the runspace
         1 {
             Write-Warning "No new process was created or a handle to it could not be obtained."
             Write-Warning "Executable was: '$($ExeAndArgs.Executable)' - this should *probably* not have happened"
