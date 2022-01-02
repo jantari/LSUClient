526bc0fc1caa5e960cabc9366005441ca76a6028
jantari <jantari@github.com>
2022-01-02 23:19:21 +0100

    add tests for Resolve-CmdVariable and Split-ExecutableAndArguments

---
+++ tests/Resolve-CmdVariable.Tests.ps1
@@ -0,0 +1,17 @@
+﻿BeforeAll {
+    # Function to test
+    . "$PSScriptRoot/../private/Resolve-CmdVariable.ps1"
+}
+
+Describe 'Resolve-CmdVariable' {
+    It 'Correctly inserts variable values' {
+        $resolved = Resolve-CmdVariable -String 'String with an inserted "%TESTVARIABLE%"' -ExtraVariables @{'TESTVARIABLE' = 'VALUE'}
+
+        $resolved | Should -BeExactly 'String with an inserted "VALUE"'
+    }
+    It "Undefined variable isn't replaced" {
+        $resolved = Resolve-CmdVariable -String 'String with an inserted "%TESTVARIABLE%"' -ExtraVariables @{}
+
+        $resolved | Should -BeExactly 'String with an inserted "%TESTVARIABLE%"'
+    }
+}
---
+++ tests/Split-ExecutableAndArguments.Tests.ps1
@@ -0,0 +1,31 @@
+﻿BeforeAll {
+    # Function to test
+    . "$PSScriptRoot/../private/Split-ExecutableAndArguments.ps1"
+}
+
+Describe 'Split-ExecutableAndArguments' {
+    It 'File that exists by absolute path' {
+        $split = Split-ExecutableAndArguments -Command "$env:SystemRoot\System32\ipconfig.exe -all" -WorkingDirectory "$env:SystemRoot\System32"
+
+        $split | Should -Not -Be $null
+        $split.Executable | Should -BeLike '*ipconfig.exe'
+        $split.Arguments | Should -BeExactly '-all'
+    }
+    It 'File that exists by relative path' {
+        $split = Split-ExecutableAndArguments -Command "ipconfig.exe -all" -WorkingDirectory "$env:SystemRoot\System32"
+
+        $split | Should -Not -Be $null
+        $split.Executable | Should -BeLike '*ipconfig.exe'
+        $split.Arguments | Should -BeExactly '-all'
+    }
+    It "File that doesn't exist by absolute path" {
+        $split = Split-ExecutableAndArguments -Command "$env:SystemRoot\doesntexist.mock" -WorkingDirectory "$env:SystemRoot\System32"
+
+        $split | Should -Be $null
+    }
+    It "File that doesn't exist by relative path" {
+        $split = Split-ExecutableAndArguments -Command "doesntexist.mock" -WorkingDirectory "$env:SystemRoot\System32"
+
+        $split | Should -Be $null
+    }
+}
