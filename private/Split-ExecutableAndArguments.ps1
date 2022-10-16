92fba9543842717331389cb7ebc1f4a380956c08
jantari <jantari@github.com>
2022-10-16 17:09:02 +0200

    revert 51a79de to not risk launching a wrong executable
    unconditionally removing pathParts from the beginning of a
    command could lead to a wrong file being executed, especially
    if the intended file cannot be found (e.g. failed download)
    and now that we're also searching for executables on PATH.
    
    Attempting to parse cmd START commands is really complex and
    would result in the same, if not more problems. Revisit later.

--- private/Split-ExecutableAndArguments.ps1
+++ private/Split-ExecutableAndArguments.ps1
@@ -22,53 +22,50 @@
 
     $pathParts = $Command -split ' '
 
-    # If necessary, also try removing parts from the start of the string
-    # This loop will rarely run more than 1 iteration, but commands can
-    # start with "START /WAIT ..." for example, see issue #57
-    for ($start = 0; $start -lt $pathParts.Count; $start++) {
-        # Repeatedly remove parts of the string from the end and test
-        for ($end = $pathParts.Count - 1; $end -ge $start; $end--) {
-            $testPath = [String]::Join(' ', $pathParts[$start..$end])
+    # Repeatedly remove parts of the string from the end and test
+    for ($end = $pathParts.Count - 1; $end -ge 0; $end--) {
+        $testPath = [String]::Join(' ', $pathParts[0..$end])
 
-            # We have to trim quotes because they mess up GetFullPath() and Join-Path
-            $testPath = $testPath.Trim('"')
+        # We have to trim quotes because they mess up GetFullPath() and Join-Path
+        $testPath = $testPath.Trim('"')
 
-            if ( [System.IO.File]::Exists($testPath) ) {
+        if ( [System.IO.File]::Exists($testPath) ) {
+            return @(
+                [System.IO.Path]::GetFullPath($testPath),
+                "$($pathParts | Select-Object -Skip ($end + 1))"
+            )
+        }
+
+        $testPathRelative = Join-Path -Path $WorkingDirectory -ChildPath $testPath
+
+        if ( [System.IO.File]::Exists($testPathRelative) ) {
+            return @(
+                [System.IO.Path]::GetFullPath($testPathRelative),
+                "$($pathParts | Select-Object -Skip ($end + 1))"
+            )
+        }
+    }
+
+    # Some commands call/rely on executables in PATH and even call
+    # them without their file extension (see issue #57). To support this
+    # we also have to search PATH with PATHEXT for potential file matches
+    $testPath = $pathParts[0].Trim('"')
+
+    foreach ($MachinePathDir in $MachinePathDirectories) {
+        $testPathInPath = Join-Path -Path $MachinePathDir -ChildPath $testPath
+        if ([System.IO.File]::Exists($testPathInPath)) {
+            return @(
+                [System.IO.Path]::GetFullPath($testPathInPath),
+                "$($pathParts | Select-Object -Skip 1)"
+            )
+        }
+        foreach ($FileExtension in $MachinePathExtensions) {
+            $testPathInPathWithExt = $testPathInPath + $FileExtension
+            if ([System.IO.File]::Exists($testPathInPathWithExt)) {
                 return @(
-                    [System.IO.Path]::GetFullPath($testPath),
-                    "$($pathParts | Select-Object -Skip ($end + 1))"
+                    [System.IO.Path]::GetFullPath($testPathInPathWithExt),
+                    "$($pathParts | Select-Object -Skip 1)"
                 )
-            }
-
-            $testPathRelative = Join-Path -Path $WorkingDirectory -ChildPath $testPath
-
-            if ( [System.IO.File]::Exists($testPathRelative) ) {
-                return @(
-                    [System.IO.Path]::GetFullPath($testPathRelative),
-                    "$($pathParts | Select-Object -Skip ($end + 1))"
-                )
-            }
-
-            # Some commands call/rely on executables in PATH and even call
-            # them without their file extension (see issue #57). To support this
-            # we also have to search PATH with PATHEXT for potential file matches
-            foreach ($MachinePathDir in $MachinePathDirectories) {
-                $testPathInPath = Join-Path -Path $MachinePathDir -ChildPath $testPath
-                if ([System.IO.File]::Exists($testPathInPath)) {
-                    return @(
-                        [System.IO.Path]::GetFullPath($testPathinPath),
-                        "$($pathParts | Select-Object -Skip ($end + 1))"
-                    )
-                }
-                foreach ($FileExtension in $MachinePathExtensions) {
-                    $testPathInPathWithExt = $testPathInPath + $FileExtension
-                    if ([System.IO.File]::Exists($testPathInPathWithExt)) {
-                        return @(
-                            [System.IO.Path]::GetFullPath($testPathInPathWithExt),
-                            "$($pathParts | Select-Object -Skip ($end + 1))"
-                        )
-                    }
-                }
             }
         }
     }
