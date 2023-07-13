jantari/LSUClient

commit 2c4616130256ba931485bb0ad1a6c8648a1be93a

relax type constraint on SourceFile parameter for #78
and unify some casing

public/Save-LSUpdate.ps1
@@ -35,9 +35,9 @@
     [CmdletBinding()]
     Param (
         [Parameter( Position = 0, ValueFromPipeline = $true, Mandatory = $true )]
-        [pscustomobject]$Package,
+        [PSCustomOobject]$Package,
         [Uri]$Proxy = $script:LSUClientConfiguration.Proxy,
-        [pscredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
+        [PSCredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
         [switch]$ProxyUseDefaultCredentials = $script:LSUClientConfiguration.ProxyUseDefaultCredentials,
         [switch]$ShowProgress,
         [switch]$Force,

private/Expand-LSUpdate.ps1
@@ -1,7 +1,7 @@
 ﻿function Expand-LSUpdate {
     Param (
         [Parameter( Mandatory = $true )]
-        [pscustomobject]$Package,
+        [PSCustomObject]$Package,
         [Parameter( Mandatory = $true )]
         [ValidateScript({ Test-Path -LiteralPath $_ -PathType Container })]
         [string]$WorkingDirectory

private/New-WebClient.ps1
@@ -8,7 +8,7 @@
     [OutputType('System.Net.WebClient')]
     Param (
         [Uri]$Proxy,
-        [pscredential]$ProxyCredential,
+        [PSCredential]$ProxyCredential,
         [switch]$ProxyUseDefaultCredentials
     )
 

private/Save-PackageFile.ps1
@@ -6,16 +6,19 @@
         downloaded, if it is a filesystem file it is copied to the destination.
 
         Saving directories recursively with this function is not supported.
+
+        .PARAMETER SourceFile
+        A PackageFilePointer object
     #>
     [CmdletBinding()]
     [OutputType('System.String')]
     Param (
         [Parameter( Mandatory = $true )]
-        [PackageFilePointer]$SourceFile,
+        [PSCustomObject]$SourceFile,
         [Parameter( Mandatory = $true )]
         [string]$Directory,
         [Uri]$Proxy,
-        [pscredential]$ProxyCredential,
+        [PSCredential]$ProxyCredential,
         [switch]$ProxyUseDefaultCredentials
     )
 

public/Get-LSUpdate.ps1
@@ -79,7 +79,7 @@
     Param (
         [string]$Model,
         [Uri]$Proxy = $script:LSUClientConfiguration.Proxy,
-        [pscredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
+        [PSCredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
         [switch]$ProxyUseDefaultCredentials = $script:LSUClientConfiguration.ProxyUseDefaultCredentials,
         [switch]$All,
         [switch]$IncludePhantomDevices,

public/Install-LSUpdate.ps1
@@ -34,12 +34,12 @@
     [OutputType('PackageInstallResult')]
     Param (
         [Parameter( Position = 0, ValueFromPipeline = $true, Mandatory = $true )]
-        [pscustomobject]$Package,
+        [PSCustomObject]$Package,
         [ValidateScript({ Test-Path -LiteralPath $_ -PathType Container })]
         [System.IO.DirectoryInfo]$Path = "$env:TEMP\LSUPackages",
         [switch]$SaveBIOSUpdateInfoToRegistry,
         [Uri]$Proxy = $script:LSUClientConfiguration.Proxy,
-        [pscredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
+        [PSCredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
         [switch]$ProxyUseDefaultCredentials = $script:LSUClientConfiguration.ProxyUseDefaultCredentials
     )
 

commit a6dacd90e2de27d0e0b1b40b386430b0bdb0c568

fix typo introduced in 2c46161

public/Save-LSUpdate.ps1
@@ -35,7 +35,7 @@
     [CmdletBinding()]
     Param (
         [Parameter( Position = 0, ValueFromPipeline = $true, Mandatory = $true )]
-        [PSCustomOobject]$Package,
+        [PSCustomObject]$Package,
         [Uri]$Proxy = $script:LSUClientConfiguration.Proxy,
         [PSCredential]$ProxyCredential = $script:LSUClientConfiguration.ProxyCredential,
         [switch]$ProxyUseDefaultCredentials = $script:LSUClientConfiguration.ProxyUseDefaultCredentials,