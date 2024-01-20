2381f5a10434c2d243c0fad1ec2826f632c6e062
jantari <jantari@outlook.de>
2024-01-20 16:42:09 +0100

    comment and simplify flow a bit after 485be02

--- private/Resolve-XMLDependencies.ps1
+++ private/Resolve-XMLDependencies.ps1
@@ -23,38 +23,42 @@
             Write-Debug "$('- ' * $XMLTreeDepth)Switched state to: $ParserState"
         }
 
-        $Result = if ($XMLTREE.SchemaInfo.Name -like "_*") {
-            switch (Test-MachineSatisfiesDependency -Dependency $XMLTREE -PackagePath $PackagePath -DebugIndent $XMLTreeDepth -FailInboxDrivers:$FailInboxDrivers) {
-                0 {
-                    $true
-                }
-                -1 {
-                    $false
-                }
-                -2 {
-                    Write-Debug "$('- ' * $XMLTreeDepth)Something unsupported encountered in: $($XMLTREE.SchemaInfo.Name)"
-                    if ($TreatUnsupportedAsPassed) { $true } else { $false }
+        $Result = switch -Wildcard ($XMLTREE.SchemaInfo.Name) {
+            '_*' {
+                switch (Test-MachineSatisfiesDependency -Dependency $XMLTREE -PackagePath $PackagePath -DebugIndent $XMLTreeDepth -FailInboxDrivers:$FailInboxDrivers) {
+                    0 {
+                        $true
+                    }
+                    -1 {
+                        $false
+                    }
+                    -2 {
+                        Write-Debug "$('- ' * $XMLTreeDepth)Something unsupported encountered in: $($XMLTREE.SchemaInfo.Name)"
+                        if ($TreatUnsupportedAsPassed) { $true } else { $false }
+                    }
                 }
             }
-        } else {
-            $SubtreeResults = Resolve-XMLDependencies -XMLIN $XMLTREE.ChildNodes -PackagePath $PackagePath -TreatUnsupportedAsPassed:$TreatUnsupportedAsPassed -FailInboxDrivers:$FailInboxDrivers -ParentNodeIsAnd:($XMLTREE.SchemaInfo.Name -eq 'And')
-            switch ($XMLTREE.SchemaInfo.Name) {
-                'And' {
-                    Write-Debug "$('- ' * $XMLTreeDepth)Tree was AND: Results: $subtreeresults"
-                    if ($subtreeresults -contains $false) { $false } else { $true  }
-                }
-                default {
-                    Write-Debug "$('- ' * $XMLTreeDepth)Tree was OR: Results: $subtreeresults"
-                    if ($subtreeresults -contains $true ) { $true  } else { $false }
-                }
+            'And' {
+                $SubtreeResults = Resolve-XMLDependencies -XMLIN $XMLTREE.ChildNodes -PackagePath $PackagePath -TreatUnsupportedAsPassed:$TreatUnsupportedAsPassed -FailInboxDrivers:$FailInboxDrivers -ParentNodeIsAnd
+                Write-Debug "$('- ' * $XMLTreeDepth)Tree was AND: Results: $SubtreeResults"
+                if ($SubtreeResults -contains $false) { $false } else { $true  }
+            }
+            default {
+                $SubtreeResults = Resolve-XMLDependencies -XMLIN $XMLTREE.ChildNodes -PackagePath $PackagePath -TreatUnsupportedAsPassed:$TreatUnsupportedAsPassed -FailInboxDrivers:$FailInboxDrivers
+                Write-Debug "$('- ' * $XMLTreeDepth)Tree was OR: Results: $SubtreeResults"
+                if ($SubtreeResults -contains $true ) { $true  } else { $false }
             }
         }
 
         Write-Debug "$('- ' * $XMLTreeDepth)< Returning $($Result -bxor $ParserState) from node $($XMLTREE.SchemaInfo.Name)"
 
         $Result -bxor $ParserState
+
+        # If we're evaluating the children of an And-node, and we get a negative result before the last child-element,
+        # we can stop and don't have to process the remaining children anymore as the And-result will always be false.
+        # This speeds things up but it can also avoid even running problematic tests, e.g. some ExternalDetections.
         if ($ParentNodeIsAnd -and $i -ne $XMLIN.Count -and -not ($Result -bxor $ParserState)) {
-            Write-Debug "$('- ' * $XMLTreeDepth)Quitting AND evaluation early because we already got one FALSE."
+            Write-Debug "$('- ' * $XMLTreeDepth)Stopping AND evaluation early"
             $ParserState = 0 # DO_HAVE
             break;
         }
