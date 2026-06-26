e65b7c17fc3326ea84bbb4885713490b587eae4c
jantari <jantari@outlook.de>
2026-06-26 23:10:52 +0200

    ci: attempt to fix var substitution test
    The Resolve-CmdVariable test suddenly started failing in GitHub CI
    as if there is now a predefined "TESTVARIABLE" on the runner VM.
    Use more random-looking variable names in the test to avoid
    conflicts.

--- tests/Resolve-CmdVariable.Tests.ps1
+++ tests/Resolve-CmdVariable.Tests.ps1
@@ -4,14 +4,14 @@
 }
 
 Describe 'Resolve-CmdVariable' {
+    It "Undefined variable isn't replaced" {
+        $resolved = Resolve-CmdVariable -String 'String with an inserted "%6A616E74617269%"' -ExtraVariables @{}
+
+        $resolved | Should -BeExactly 'String with an inserted "%6A616E74617269%"'
+    }
     It 'Correctly inserts variable values' {
-        $resolved = Resolve-CmdVariable -String 'String with an inserted "%TESTVARIABLE%"' -ExtraVariables @{'TESTVARIABLE' = 'VALUE'}
+        $resolved = Resolve-CmdVariable -String 'String with an inserted "%4C5355436C69656E74%"' -ExtraVariables @{'4C5355436C69656E74' = 'VALUE'}
 
         $resolved | Should -BeExactly 'String with an inserted "VALUE"'
-    }
-    It "Undefined variable isn't replaced" {
-        $resolved = Resolve-CmdVariable -String 'String with an inserted "%TESTVARIABLE%"' -ExtraVariables @{}
-
-        $resolved | Should -BeExactly 'String with an inserted "%TESTVARIABLE%"'
     }
 }
